71e910453fcf5f7ec3cb249ee3cba691c984c79e
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-07-12 22:13:32 -0600

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import express, { type Application } from "express";
+
+const port = process.env.PORT;
+
+const app: Application = express();
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
