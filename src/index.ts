3d43ffbf57e32d4acbece5d01bcd55f3ebb9fccb
Junior Castillo <castillojunior099@gmail.com>
2024-07-14 10:53:12 -0600

    Partial feature

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
 import express, { type Application } from "express";
+// const connection = require('./db')
 
 const port = process.env.PORT;
 
 const app: Application = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello")
+})
+
+app.delete('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.json({
+    status: 200,
+    message: `User with ID: ${userId} has been deleted`
+  })
+
+  // connection.query('DELETE FROM users WHERE id = ?', [userId], (error, results) => {
+  //   if (error){
+  //     return res.status(500).send(error)
+  //   }
+
+  //   return res.json({
+  //     status: 200,
+  //     message: `User with ID: ${userId} has been deleted`
+  //   })
+
+  // })
+
+})
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
