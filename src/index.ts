d764593f2249b08c2033d025f3bac1544459b1bb
carlos flores <carlosftru@gmail.com>
2024-07-21 18:33:26 -0600

    added session cookies for user authentication using jwt

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import express, { type Application } from "express";
+import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import { userRouter } from "./routes/users";
 import { authRouter } from "./routes/auth";
+import { verifyToken } from "./middlewares/authMiddleware";
 
-const port: String|Number = process.env.PORT || 8080;
+const port: String | Number = process.env.PORT || 8080;
 
 const app: Application = express();
 
-app.use(bodyParser.json())
-
-app.use('/user', userRouter)
 app.use(bodyParser.json());
-
 app.use("/auth", authRouter);
+
+app.use(cookieParser());
+app.use(verifyToken);
+app.use("/user", userRouter);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,11 @@
 import { Router, type Request, type Response } from "express";
-import * as bcrypt from 'bcrypt'
+import * as jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
 import { eq } from "drizzle-orm";
 import { z, ZodError } from "zod";
 import { db } from "../db/db";
 import { users, type User } from "../db/schema";
+import { verifyToken } from "../middlewares/authMiddleware";
 
 export const userRouter: Router = Router();
 
@@ -13,30 +15,48 @@ const loginSchema = z.object({
 });
 
 userRouter.post("/login", async (req: Request, res: Response) => {
-  const result = loginSchema.safeParse(req.body);
-  if (!result.success) {
-    const err: ZodError = result.error;
-    return res.status(400).json({ errors: err.errors });
+  try {
+    const result = loginSchema.safeParse(req.body);
+    if (!result.success) {
+      const err: ZodError = result.error;
+      return res.status(400).json({ errors: err.errors });
+    }
+
+    const { numeroCuenta, password } = req.body;
+
+    // agregar que result es de tipo user
+    const userQuery: User[] | any = await db
+      .select()
+      .from(users)
+      .where(eq(users.numeroCuenta, numeroCuenta));
+
+    const user = userQuery[0];
+    if (!user)
+      return res.status(401).json({ msg: "User credentials are not valid" });
+
+    const matchPassword = await bcrypt.compare(password, user.password);
+
+    if (!matchPassword)
+      return res.status(401).json({ msg: "User credentials are not valid" });
+
+    // creating cookie session
+    const SECRET_KEY: string = process.env.SECRET_KEY || "prueba";
+    if (!SECRET_KEY) {
+      return res.status(500).json({ msg: "Could not authenticate user" });
+    }
+
+    const token = jwt.sign({ userId: user.numeroCuenta }, SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).cookie("access_token", token).json(user);
+  } catch (error) {
+    return res.status(500).json({ msg: "Error while login", e: error });
   }
-
-  const { numeroCuenta, password } = req.body;
-
-  // agregar que result es de tipo user
-  const userQuery: User[] | any = await db
-    .select()
-    .from(users)
-    .where(eq(users.numeroCuenta, numeroCuenta));
-
-  const user = userQuery[0]
-  if (!user)
-    return res.status(401).json({ msg: "User credentials are not valid" });
-
-  const matchPassword = await bcrypt.compare(password, user.password)
-
-  if (!matchPassword)
-    return res.status(401).json({ msg: "User credentials are not valid" });
-
-  //TODO: se deberia crear una session cookie para el usuario
-  res.status(200).json(user);
 });
 
+userRouter.use(verifyToken);
+
+userRouter.get("/home", (req: Request, res: Response) => {
+  return res.status(200).json({ msg: "YOU WERE AUTHENTICATED" });
+});
