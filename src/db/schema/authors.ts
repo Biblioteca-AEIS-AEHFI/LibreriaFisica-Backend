f39795cae2acc06b50556bd77aca3f955497e8c0
Junior Castillo <castillojunior099@gmail.com>
2024-09-19 10:42:28 -0600

    Actualizando objetos para validaciones de authors

--- src/db/schema/authors.ts
+++ src/db/schema/authors.ts
@@ -2,6 +2,7 @@ import { relations } from "drizzle-orm";
 import { int, text, mysqlTable } from "drizzle-orm/mysql-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { authorsPerBook } from "./authorsPerBooks";
+import { z, ZodError } from "zod";
 
 // Autores
 export const authors = mysqlTable("authors", {
@@ -19,3 +20,14 @@ export type Author = typeof authors.$inferSelect;
 
 export const NewAuthorSchema = createInsertSchema(authors);
 export const AuthorSchema = createSelectSchema(authors);
+
+export const PartialGetAuthor = z.object({
+  authorName: z.string().min(1, "Author name cannot be empty").optional(),
+});
+
+export const UpdateAuthorSchema = z.object({
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+}).refine((data) => data.firstName || data.lastName, {
+  message: "At least one of 'firstName' or 'lastName' must be provided",
+});
